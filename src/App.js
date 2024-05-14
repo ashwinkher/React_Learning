f4a1d30e367c693d086ba0a10c191f8387294eb0
Ashwin Kher <ashwin.kher@americaneagle.com>
2024-05-14 09:55:19 -0500

    card-component folder added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
-import {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component"
+import "./App.css";
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
 
     this.state = {
-      monsters : []
-    }
-  };
-
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users)=> this.setState(
-      ()=>{return {monsters: users};
-    },
-    ()=> {
-      console.log(this.state);
-    }
-    )
-  );
+      monsters: [],
+      searchField : ''
+    };
   }
 
-render() {
-  return (
-    <div className="App">
-      {this.state.monsters.map((monster)=>
-      {
-        return (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
-            </div> 
-        );
-      })}
-    </div>
-  );
-}
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          }
+        )
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(()=>{
+      return {searchField}; //this means searchField is the key and the value will be value of searchField
+    });
+  }
+
+  render() {
+    console.log('render from Appjs');
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} =this;
+
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLowerCase().includes(searchField);
+    })
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters.."
+          onChange={onSearchChange}
+        ></input>
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
